c897614b75c3e5f35a8b131b5a6191a79cca650f
finphie <19237170+finphie@users.noreply.github.com>
2018-09-02 10:53:13 +0900

    Depth追加

---
+++ BitbankDotNet/Api/Entities/Depth.cs
@@ -0,0 +1,21 @@
+﻿using BitbankDotNet.Formatters;
+using SpanJson;
+using System.Collections.Generic;
+
+namespace BitbankDotNet.Api.Entities
+{
+    public class Depth
+    {
+        [JsonCustomSerializer(typeof(BitbankDepthFormatter))]
+        public List<double[]> Asks { get; set; }
+        [JsonCustomSerializer(typeof(BitbankDepthFormatter))]
+        public List<double[]> Bids { get; set; }
+
+        public override string ToString()
+            => JsonSerializer.Generic.Utf16.Serialize(this);
+    }
+
+    class DepthResponse : Response<Depth>
+    {
+    }
+}
---
+++ BitbankDotNet/Formatters/BitbankDepthFormatter.cs
@@ -0,0 +1,44 @@
+﻿using SpanJson;
+using System;
+using System.Collections.Generic;
+
+namespace BitbankDotNet.Formatters
+{
+    sealed class BitbankDepthFormatter : ICustomJsonFormatter<List<double[]>>
+    {
+        public static readonly BitbankDepthFormatter Default = new BitbankDepthFormatter();
+        static readonly DoubleAsStringArrayFormatter ElementFormatter = DoubleAsStringArrayFormatter.Default;
+
+        public List<double[]> Deserialize(ref JsonReader<byte> reader)
+        {
+            var list = new List<double[]>(4);
+            reader.ReadBeginArrayOrThrow();
+            var count = 0;
+            while (!reader.TryReadIsEndArrayOrValueSeparator(ref count))
+                list.Add(ElementFormatter.Deserialize(ref reader));
+
+            return list;
+        }
+
+        public List<double[]> Deserialize(ref JsonReader<char> reader)
+        {
+            var list = new List<double[]>(4);
+            reader.ReadBeginArrayOrThrow();
+            var count = 0;
+            while (!reader.TryReadIsEndArrayOrValueSeparator(ref count))
+                list.Add(ElementFormatter.Deserialize(ref reader));
+
+            return list;
+        }
+
+        public void Serialize(ref JsonWriter<byte> writer, List<double[]> value, int nestingLimit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Serialize(ref JsonWriter<char> writer, List<double[]> value, int nestingLimit)
+        {
+            throw new NotImplementedException();
+        }     
+    }
+}
