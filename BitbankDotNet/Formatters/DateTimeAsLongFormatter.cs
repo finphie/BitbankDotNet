a295292649bf7ace1e5c2622334051dd4e0cf354
finphie <19237170+finphie@users.noreply.github.com>
2018-09-02 10:29:55 +0900

    UnixTime用DateTimeFormatter追加

---
+++ BitbankDotNet/Formatters/DateTimeAsLongFormatter.cs
@@ -0,0 +1,22 @@
+﻿using SpanJson;
+using System;
+
+namespace BitbankDotNet.Formatters
+{
+    sealed class DateTimeAsLongFormatter : ICustomJsonFormatter<DateTime>
+    {
+        public static readonly DateTimeAsLongFormatter Default = new DateTimeAsLongFormatter();
+
+        public DateTime Deserialize(ref JsonReader<byte> reader)
+            => DateTimeOffset.FromUnixTimeMilliseconds(reader.ReadUtf8Int64()).DateTime;
+
+        public DateTime Deserialize(ref JsonReader<char> reader)
+            => DateTimeOffset.FromUnixTimeMilliseconds(reader.ReadUtf16Int64()).DateTime;
+
+        public void Serialize(ref JsonWriter<byte> writer, DateTime value, int nestingLimit)
+            => writer.WriteUtf8Int64(((DateTimeOffset) value).ToUnixTimeMilliseconds());
+
+        public void Serialize(ref JsonWriter<char> writer, DateTime value, int nestingLimit)
+            => writer.WriteUtf16Int64(((DateTimeOffset) value).ToUnixTimeMilliseconds());
+    }
+}
