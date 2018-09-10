64c786b98202ef3161fb3c1624f1c983c3416043
finphie <19237170+finphie@users.noreply.github.com>
2018-09-10 17:12:13 +0900

    ユニットテスト用T4テンプレート追加
    Tickerのみ

---
+++ BitbankDotNet.CodeGenerator/BitbankClientTestTemplate1.cs
@@ -0,0 +1,19 @@
+﻿namespace BitbankDotNet.CodeGenerator
+{
+    partial class BitbankClientTestTemplate
+    {
+        public string MethodName { get; set; }
+
+        public string ApiName1 { get; set; }
+        public string ApiName2 { get; set; }
+
+        public BitbankClientTestTemplate(string methodName)
+        {
+            MethodName = methodName;
+
+            var name = methodName.Replace("Get", "").Replace("Async", "");
+            ApiName1 = name;
+            ApiName2 = name.ToLower();
+        }
+    }
+}
---
+++ BitbankDotNet.CodeGenerator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace BitbankDotNet.CodeGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string methodName = nameof(BitbankClient.GetTickerAsync);
+
+            var tt = new BitbankClientTestTemplate(methodName);
+            var text = tt.TransformText();
+            File.WriteAllText(nameof(BitbankClient) + methodName + "Test.cs", text);
+        }
+    }
+}
