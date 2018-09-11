290a59b8808ad2e4bb6eb971299f5ab931a6321d
finphie <19237170+finphie@users.noreply.github.com>
2018-09-11 12:38:32 +0900

    リフレクションを利用して初期化するように変更

--- BitbankDotNet.CodeGenerator/Program.cs
+++ BitbankDotNet.CodeGenerator/Program.cs
@@ -6,20 +6,32 @@ namespace BitbankDotNet.CodeGenerator
 {
     class Program
     {
+        static void SetValue<T>(T target)
+        {
+            var properties = typeof(T).GetProperties();
+
+            foreach (var property in properties)
+            {
+                var type = property.PropertyType;          
+                if (type == typeof(double))
+                    property.SetValue(target, 76543210.12345678);
+                else if (type == typeof(string))
+                    property.SetValue(target, "abc");
+                else if (type == typeof(DateTime))
+                    property.SetValue(target, new DateTime(2018, 1, 1, 1, 1, 1, 111));
+                else
+                    throw new NotImplementedException(type.Name);
+            }
+        }
+
         static void Main(string[] args)
         {
             const string methodName = nameof(BitbankClient.GetTickerAsync);
 
-            var json = new Ticker
-            {
-                Sell = 76543210.12345678,
-                Buy = 76543210.12345678,
-                High = 76543210.12345678,
-                Low = 76543210.12345678,
-                Last = 76543210.12345678,
-                Vol = 76543210.12345678,
-                Timestamp = new DateTime(2018, 1, 1, 1, 1, 1, 111)
-            }.ToString().Replace("\"", @"\""");
+            var ticker = new Ticker();
+            SetValue(ticker);
+
+            var json = ticker.ToString().Replace("\"", @"\""");
 
             var tt = new BitbankClientTestTemplate(json, methodName);
             var text = tt.TransformText();
