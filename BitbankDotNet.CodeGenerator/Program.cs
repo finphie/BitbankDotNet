6e5afc7d68e7f059e613db7bfc8023d1d1b8936e
finphie <19237170+finphie@users.noreply.github.com>
2018-09-11 17:14:49 +0900

    リファクタリング

--- BitbankDotNet.CodeGenerator/Program.cs
+++ BitbankDotNet.CodeGenerator/Program.cs
@@ -1,32 +1,10 @@
 ﻿using BitbankDotNet.Entities;
-using System;
 using System.IO;
 
 namespace BitbankDotNet.CodeGenerator
 {
     class Program
     {
-        static object GetTestValue<T>(T property)
-        {
-            switch (property)
-            {
-                case double _:
-                    return 76543210.12345678;
-                case string _:
-                    return "abc";
-                case DateTime _:
-                    return new DateTime(2018, 1, 1, 1, 1, 1, 111);
-                default:
-                    throw new NotImplementedException(typeof(T).Name);
-            }
-        }
-
-        static void SetValue<T>(T target)
-        {
-            foreach (var property in typeof(T).GetProperties())
-                property.SetValue(target, GetTestValue(property.GetValue(target)));
-        }
-
         static void Main()
         {
             const string methodName = nameof(BitbankClient.GetTickerAsync);
