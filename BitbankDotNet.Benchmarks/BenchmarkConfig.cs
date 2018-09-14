02c27a90d1b0b9906f7c19a38746afa1052a9fb9
finphie <19237170+finphie@users.noreply.github.com>
2018-09-14 18:49:20 +0900

    .Net Core 3.0とCoreRTのベンチマーク設定をコメントアウト

--- BitbankDotNet.Benchmarks/BenchmarkConfig.cs
+++ BitbankDotNet.Benchmarks/BenchmarkConfig.cs
@@ -18,17 +18,16 @@ namespace BitbankDotNet.Benchmarks
             Add(Job.Default.With(CsProjCoreToolchain.NetCoreApp21));
             //Add(Job.Default.With(CsProjCoreToolchain.NetCoreApp22));
 
-            Add(Job.Default.With(
-                CsProjCoreToolchain.From(
-                    new NetCoreAppSettings("netcoreapp3.0", "3.0.0-*", ".NET Core 3.0")))
-            );
+            //Add(Job.Default.With(
+            //    CsProjCoreToolchain.From(
+            //        new NetCoreAppSettings("netcoreapp3.0", "3.0.0-*", ".NET Core 3.0"))));
 
             // CoreRT（RyuJIT利用）
             // cf. https://benchmarkdotnet.org/articles/configs/toolchains.html
             // cf. https://github.com/dotnet/corert/blob/master/Documentation/how-to-build-and-run-ilcompiler-in-console-shell-prompt.md
-            Add(Job.Default
-                .With(CsProjCoreToolchain.NetCoreApp21)
-                .With(Runtime.CoreRT));
+            //Add(Job.Default
+            //    .With(CsProjCoreToolchain.NetCoreApp21)
+            //    .With(Runtime.CoreRT));
 
             // CoreRT（CPP Code Generator利用）
             //Add(Job.Default
