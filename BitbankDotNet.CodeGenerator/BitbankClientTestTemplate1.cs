7f8280336dfb1b122fb9b780c03904be46b4c85b
finphie <19237170+finphie@users.noreply.github.com>
2018-09-16 14:56:54 +0900

    型を取得できていなかった問題を修正
    引数propertyにnullが渡されることがあったため

--- BitbankDotNet.CodeGenerator/BitbankClientTestTemplate1.cs
+++ BitbankDotNet.CodeGenerator/BitbankClientTestTemplate1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.CSharp;
+﻿using BitbankDotNet.Entities;
+using Microsoft.CSharp;
 using System;
 using System.CodeDom;
 using System.Linq;
@@ -48,25 +49,24 @@ namespace BitbankDotNet.CodeGenerator
         static string GetParameterString(MethodBase method)
             => string.Join(", ", method.GetParameters().Select(p => $"{GetTypeOutput(p.ParameterType)} {p.Name}"));
 
-        static object GetTestValue(object property)
+        static object GetTestValue(Type type)
         {
-            switch (property)
-            {
-                case double _:
-                    return 76543210.12345678;
-                case int _:
-                    return int.MaxValue;
-                case long _:
-                    return long.MaxValue;
-                case string _:
-                    return "abc";
-                case DateTime _:
-                    return new DateTime(2018, 1, 1, 1, 1, 1, 111);
-                case AssetName _:
-                    return AssetName.Jpy;
-                default:
-                    throw new NotImplementedException(property.GetType().Name);
-            }
+            if (type == typeof(double))
+                return 76543210.12345678;
+            if (type == typeof(int))
+                return int.MaxValue;
+            if (type == typeof(long))
+                return long.MaxValue;
+            if (type == typeof(string))
+                return "abc";
+            if (type == typeof(DateTime))
+                return new DateTime(2018, 1, 1, 1, 1, 1, 111);
+            if (type == typeof(BoardOrder[]))
+                return new[] {new BoardOrder {Price = 1.1, Amount = 1.2}, new BoardOrder {Price = 1.3, Amount = 1.4}};
+            if (type == typeof(AssetName))
+                return AssetName.Jpy;
+
+            throw new NotImplementedException(type.Name);
         }
 
         static string GetTestValueString(object property)
@@ -106,7 +106,7 @@ namespace BitbankDotNet.CodeGenerator
         static void SetValue(object target)
         {
             foreach (var property in target.GetType().GetProperties())
-                property.SetValue(target, GetTestValue(property.GetValue(target)));
+                property.SetValue(target, GetTestValue(property.PropertyType));
         }
     }
 }
