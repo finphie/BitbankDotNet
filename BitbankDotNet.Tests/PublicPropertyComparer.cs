59e5746d8d14e8ab3ec0ce9e7bb8f88cf8d2156f
finphie <19237170+finphie@users.noreply.github.com>
2018-09-16 17:27:22 +0900

    Comparerクラス追加

---
+++ BitbankDotNet.Tests/PublicPropertyComparer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace BitbankDotNet.Tests
+{
+    class PublicPropertyComparer<T> : IEqualityComparer<T>
+    {
+        public bool Equals(T expected, T actual)
+            => typeof(T)
+                .GetProperties(
+                    BindingFlags.DeclaredOnly |
+                    BindingFlags.Instance |
+                    BindingFlags.Public |
+                    BindingFlags.Static)
+                .All(p => p.GetValue(expected).Equals(p.GetValue(actual)));
+
+        public int GetHashCode(T obj)
+            => obj.GetHashCode();
+    }
+}
