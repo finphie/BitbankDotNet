4edfa8435c85c5846b4e50568c3dd1ab57f24811
finphie <19237170+finphie@users.noreply.github.com>
2018-09-17 18:51:37 +0900

    ユニットテストでのdoubleの比較を修正

--- BitbankDotNet.Tests/PublicPropertyComparer.cs
+++ BitbankDotNet.Tests/PublicPropertyComparer.cs
@@ -13,7 +13,18 @@ namespace BitbankDotNet.Tests
                     BindingFlags.Instance |
                     BindingFlags.Public |
                     BindingFlags.Static)
-                .All(p => p.GetValue(expected).Equals(p.GetValue(actual)));
+                .All(p =>
+                {
+                    // double型のまま比較すると精度の関係でfalseになる。
+                    // また、expectedとactualの差の絶対値で許容範囲を決めようにも、
+                    // Entityクラス毎に有効桁数が変わるため、対応が難しい。
+                    // 従って、stringかdecimalを利用して比較する必要がある。
+                    // stringでは、CultureInfoを考慮する必要があるため大変。
+                    if (p.GetValue(expected) is double e && p.GetValue(actual) is double a)
+                        return (decimal) e == (decimal) a;
+
+                    return p.GetValue(expected).Equals(p.GetValue(actual));
+                });
 
         public int GetHashCode(T obj)
             => obj.GetHashCode();
