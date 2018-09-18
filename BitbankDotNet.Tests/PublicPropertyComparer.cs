8f030c40957a4f031f04c36f777822635fa60528
finphie <19237170+finphie@users.noreply.github.com>
2018-09-18 14:25:06 +0900

    ユニットテストでのEntity配列比較処理を修正
    配列の場合は、ToString()した文字列に対して比較する。

--- BitbankDotNet.Tests/PublicPropertyComparer.cs
+++ BitbankDotNet.Tests/PublicPropertyComparer.cs
@@ -15,15 +15,20 @@ namespace BitbankDotNet.Tests
                     BindingFlags.Static)
                 .All(p =>
                 {
+                    var expectedValue = p.GetValue(expected);
+                    var actualValue = p.GetValue(actual);
+
                     // double型のまま比較すると精度の関係でfalseになる。
                     // また、expectedとactualの差の絶対値で許容範囲を決めようにも、
                     // Entityクラス毎に有効桁数が変わるため、対応が難しい。
                     // 従って、stringかdecimalを利用して比較する必要がある。
                     // stringでは、CultureInfoを考慮する必要があるため大変。
-                    if (p.GetValue(expected) is double e && p.GetValue(actual) is double a)
+                    if (expectedValue is double e && actualValue is double a)
                         return (decimal) e == (decimal) a;
 
-                    return p.GetValue(expected).Equals(p.GetValue(actual));
+                    return expectedValue.GetType().IsArray
+                        ? ((object[]) expectedValue).ToString() == ((object[]) actualValue).ToString()
+                        : p.GetValue(expected).Equals(p.GetValue(actual));
                 });
 
         public int GetHashCode(T obj)
