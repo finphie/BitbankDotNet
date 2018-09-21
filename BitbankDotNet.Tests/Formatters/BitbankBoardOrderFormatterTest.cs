2e25852b23fd75e679c735b1aee16b22a8161468
finphie <19237170+finphie@users.noreply.github.com>
2018-09-21 12:15:54 +0900

    BoardOrderFormatterのテスト追加

---
+++ BitbankDotNet.Tests/Formatters/BitbankBoardOrderFormatterTest.cs
@@ -0,0 +1,59 @@
+﻿using BitbankDotNet.Entities;
+using BitbankDotNet.Resolvers;
+using System.Text;
+using Xunit;
+using static SpanJson.JsonSerializer.Generic.Utf16;
+using static SpanJson.JsonSerializer.Generic.Utf8;
+
+namespace BitbankDotNet.Tests.Formatters
+{
+    public class BitbankBoardOrderFormatterTest
+    {
+        static readonly BoardOrder Entity = new BoardOrder
+        {
+            Price = 0.1,
+            Amount = 1.2
+        };
+
+        const string Json = "[\"0.1\",\"1.2\"]";
+        static readonly byte[] UJson = Encoding.UTF8.GetBytes(Json);
+
+        [Fact]
+        public void Deserialize_UTF8のJSON文字列を入力_BoardOrderを返す()
+        {
+            var deserialize = Deserialize<BoardOrder, BitbankResolver<byte>>(UJson);
+
+            Assert.NotNull(deserialize);
+            Assert.Equal(Entity.Price, deserialize.Price);
+            Assert.Equal(Entity.Amount, deserialize.Amount);
+        }
+
+        [Fact]
+        public void Deserialize_UTF16のJSON文字列を入力_BoardOrderを返す()
+        {
+            var deserialize = Deserialize<BoardOrder, BitbankResolver<char>>(Json);
+
+            Assert.NotNull(deserialize);
+            Assert.Equal(Entity.Price, deserialize.Price);
+            Assert.Equal(Entity.Amount, deserialize.Amount);
+        }
+
+        [Fact]
+        public void Serialize_BoardOrderを入力_UTF8のJSON文字列を出力()
+        {
+            var serialize = Serialize<BoardOrder, BitbankResolver<byte>>(Entity);
+
+            Assert.NotNull(serialize);
+            Assert.Equal(UJson, serialize);
+        }
+
+        [Fact]
+        public void Serialize_BoardOrderを入力_UTF16のJSON文字列を出力()
+        {
+            var serialize = Serialize<BoardOrder, BitbankResolver<char>>(Entity);
+
+            Assert.NotNull(serialize);
+            Assert.Equal(Json, serialize);
+        }
+    }
+}
