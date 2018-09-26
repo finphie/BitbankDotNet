cc2d0526901e326d7b1d7afe9282381d7aa772d9
finphie <19237170+finphie@users.noreply.github.com>
2018-09-26 10:35:49 +0900

    配列の場合でも各プロパティを比較するように変更

--- BitbankDotNet.Tests/PublicPropertyComparer.cs
+++ BitbankDotNet.Tests/PublicPropertyComparer.cs
@@ -7,31 +7,38 @@ namespace BitbankDotNet.Tests
     class PublicPropertyComparer<T> : IEqualityComparer<T>
     {
         public bool Equals(T expected, T actual)
-            => typeof(T)
-                .GetProperties(
+            => PublicPropertyEquals(expected, actual);
+
+        static bool PublicPropertyEquals(object expected, object actual)
+            => expected.GetType().GetProperties(
                     BindingFlags.DeclaredOnly |
                     BindingFlags.Instance |
                     BindingFlags.Public |
                     BindingFlags.Static)
-                .All(p =>
-                {
-                    var expectedValue = p.GetValue(expected);
-                    var actualValue = p.GetValue(actual);
+                .All(p => PublicPropertyEqualsCore(p.GetValue(expected), p.GetValue(actual)));
 
-                    // double型のまま比較すると精度の関係でfalseになる。
-                    // また、expectedとactualの差の絶対値で許容範囲を決めようにも、
-                    // Entityクラス毎に有効桁数が変わるため、対応が難しい。
-                    // 従って、stringかdecimalを利用して比較する必要がある。
-                    // stringでは、CultureInfoを考慮する必要があるため大変。
-                    if (expectedValue is double e && actualValue is double a)
-                        return (decimal) e == (decimal) a;
+        static bool PublicPropertyEqualsCore(object expected, object actual)
+        {
+            // double型のまま比較すると精度の関係でfalseになる。
+            // また、expectedとactualの差の絶対値で許容範囲を決めようにも、
+            // Entityクラス毎に有効桁数が変わるため、対応が難しい。
+            // 従って、stringかdecimalを利用して比較する必要がある。
+            // stringでは、CultureInfoを考慮する必要があるため大変。
+            if (expected is double e && actual is double a)
+                return (decimal)e == (decimal)a;
 
-                    return expectedValue.GetType().IsArray
-                        ? ((object[]) expectedValue).ToString() == ((object[]) actualValue).ToString()
-                        : p.GetValue(expected).Equals(p.GetValue(actual));
-                });
+            return expected.GetType().IsArray
+                ? ((object[]) expected, (object[]) actual).Zip().All(t => PublicPropertyEquals(t.first, t.second))
+                : expected.Equals(actual);
+        }
 
         public int GetHashCode(T obj)
             => obj.GetHashCode();
+    }
+
+    static class ValueTupleExtensions
+    {
+        public static IEnumerable<(TFirst first, TSecond second)> Zip<TFirst, TSecond>(this (IEnumerable<TFirst> first, IEnumerable<TSecond> second) source)
+            => source.first.Zip(source.second, (x1, x2) => (x1, x2));
     }
 }
