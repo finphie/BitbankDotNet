f0dd86a9a3ecf8f048cfa19a5825a2183cddc206
finphie <19237170+finphie@users.noreply.github.com>
2018-10-07 15:33:08 +0900

    doubleの文字列変換ベンチマークを追加

---
+++ BitbankDotNet.Benchmarks/DoubleFormattingBenchmark.cs
@@ -0,0 +1,108 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Buffers.Text;
+using System.Globalization;
+using System.Text;
+
+namespace BitbankDotNet.Benchmarks
+{
+    /// <summary>
+    /// doubleをstringに変換
+    /// </summary>
+    /// <remarks>
+    /// 
+    /// </remarks>
+    [Config(typeof(BenchmarkConfig))]
+    public class DoubleFormattingBenchmark
+    {
+        const int BufferSize = 32;
+        // ReSharper disable once ConvertToConstant.Local
+        // ReSharper disable once FieldCanBeMadeReadOnly.Local
+        double _value = 12345.6789;
+
+        [Benchmark]
+        public string DoubleToString()
+            => _value.ToString(CultureInfo.InvariantCulture);
+
+        [Benchmark]
+        public byte[] DoubleToStringEncodingGetBytes()
+            => Encoding.UTF8.GetBytes(_value.ToString(CultureInfo.InvariantCulture));
+
+        [Benchmark]
+        public byte[] DoubleToStringEncodingGetBytesSpan()
+        {
+            Span<byte> byteBuffer = stackalloc byte[BufferSize];
+            var byteLength = Encoding.UTF8.GetBytes(_value.ToString(CultureInfo.InvariantCulture), byteBuffer);
+            return byteBuffer.Slice(0, byteLength).ToArray();
+        }
+
+        [Benchmark]
+        public unsafe byte[] DoubleToStringEncodingGetBytesUnsafe()
+        {
+            var value = _value.ToString(CultureInfo.InvariantCulture);
+            Span<byte> byteBuffer = stackalloc byte[BufferSize];
+            fixed (char* chars = value)
+            fixed (byte* bytes = byteBuffer)
+            {
+                var byteLength = Encoding.UTF8.GetBytes(chars, value.Length, bytes, BufferSize);
+                return byteBuffer.Slice(0, byteLength).ToArray();
+            }
+        }
+
+        [Benchmark]
+        public string DoubleTryFormat()
+        {
+            Span<char> charBuffer = stackalloc char[BufferSize];
+            _value.TryFormat(charBuffer, out var charLength, null, CultureInfo.InvariantCulture);
+            return charBuffer.Slice(0, charLength).ToString();
+        }
+
+        [Benchmark]
+        public byte[] DoubleTryFormatEncodingGetBytes()
+        {
+            Span<char> charBuffer = stackalloc char[BufferSize];
+            _value.TryFormat(charBuffer, out var charLength, null, CultureInfo.InvariantCulture);
+            return Encoding.UTF8.GetBytes(charBuffer.Slice(0, charLength).ToArray());
+        }
+
+        [Benchmark]
+        public byte[] DoubleTryFormatEncodingGetBytesSpan()
+        {
+            Span<char> charBuffer = stackalloc char[BufferSize];
+            _value.TryFormat(charBuffer, out var charLength, null, CultureInfo.InvariantCulture);
+            Span<byte> byteBuffer = stackalloc byte[charLength];
+            var byteLength = Encoding.UTF8.GetBytes(charBuffer.Slice(0, charLength), byteBuffer);
+            return byteBuffer.Slice(0, byteLength).ToArray();
+        }
+
+        [Benchmark]
+        public unsafe byte[] DoubleTryFormatEncodingGetBytesUnsafe()
+        {
+            Span<char> charBuffer = stackalloc char[BufferSize];
+            _value.TryFormat(charBuffer, out var charLength, null, CultureInfo.InvariantCulture);
+            Span<byte> byteBuffer = stackalloc byte[charLength];
+            fixed (char* chars = charBuffer)
+            fixed (byte* bytes = byteBuffer)
+            {
+                var byteLength = Encoding.UTF8.GetBytes(chars, charLength, bytes, BufferSize);
+                return byteBuffer.Slice(0, byteLength).ToArray();
+            }
+        }
+
+        [Benchmark]
+        public string Utf8FormatterTryFormatEncodingGetString()
+        {
+            Span<byte> byteBuffer = stackalloc byte[BufferSize];
+            Utf8Formatter.TryFormat(_value, byteBuffer, out var byteLength);
+            return Encoding.UTF8.GetString(byteBuffer.Slice(0, byteLength));
+        }
+
+        [Benchmark]
+        public byte[] Utf8FormatterTryFormat()
+        {
+            Span<byte> byteBuffer = stackalloc byte[BufferSize];
+            Utf8Formatter.TryFormat(_value, byteBuffer, out var byteLength);
+            return byteBuffer.Slice(0, byteLength).ToArray();
+        }
+    }
+}
