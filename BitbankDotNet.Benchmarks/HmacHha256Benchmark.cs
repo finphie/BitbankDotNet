96d98ba992dad0321237094417318d4f240cd5fb
finphie <19237170+finphie@users.noreply.github.com>
2018-10-22 09:56:26 +0900

    HMACSHA256のベンチマークを修正
    高速化など

--- BitbankDotNet.Benchmarks/HmacHha256Benchmark.cs
+++ BitbankDotNet.Benchmarks/HmacHha256Benchmark.cs
@@ -8,52 +8,58 @@ namespace BitbankDotNet.Benchmarks
     [Config(typeof(BenchmarkConfig))]
     public class HmacHha256Benchmark
     {
-        byte[] _key;
+        // キーの長さは64文字固定
+        const int KeyLength = 64;
+
+        readonly HMACSHA256 _hmac;
+        readonly IncrementalHash _incrementalHash;
+
         byte[] _message;
 
         [Params(32, 64, 128)]
-        public int MessageLength;
+        public int MessageLength { get; set; }
+
+        public HmacHha256Benchmark()
+        {
+            var key = CreateUtf8Bytes(KeyLength);
+            _hmac = new HMACSHA256(key);
+            _incrementalHash = IncrementalHash.CreateHMAC(HashAlgorithmName.SHA256, key);
+        }
 
         [GlobalSetup]
-        public void Setup()
+        public void Setup() => _message = CreateUtf8Bytes(MessageLength);
+
+        [GlobalCleanup]
+        public void Cleanup()
         {
-            // UTF-8 byte配列作成
-            byte[] CreateUtf8Bytes(int length)
-            {
-                // UTF-16文字列作成
-                string CreateUtf16String() => Guid.NewGuid().ToString("N");
-
-                var sb = new StringBuilder(64);
-                sb.Append(CreateUtf16String());
-
-                var count = length / sb.Length;
-                for (var i = 0; i < count; i++)
-                    sb.Append(CreateUtf16String());
-
-                return Encoding.UTF8.GetBytes(sb.ToString().Substring(0, length));
-            }
-
-            // キーは64文字固定
-            _key = CreateUtf8Bytes(64);
-
-            _message = CreateUtf8Bytes(MessageLength);
+            _hmac?.Dispose();
+            _incrementalHash?.Dispose();
         }
 
         [Benchmark]
-        public byte[] HmacHha256ComputeHash()
-        {
-            using (var hmac = new HMACSHA256(_key))
-                return hmac.ComputeHash(_message);
-        }
+        public byte[] HmacHha256ComputeHash() => _hmac.ComputeHash(_message);
 
         [Benchmark]
         public byte[] IncrementalHashCreateHmac()
-        {            
-            using (var incrementalHash = IncrementalHash.CreateHMAC(HashAlgorithmName.SHA256, _key))
-            {
-                incrementalHash.AppendData(_message);
-                return incrementalHash.GetHashAndReset();
-            }            
+        {
+            _incrementalHash.AppendData(_message);
+            return _incrementalHash.GetHashAndReset();
+        }
+
+        // UTF-8 byte配列作成
+        static byte[] CreateUtf8Bytes(int length)
+        {
+            // UTF-16文字列作成
+            string CreateUtf16String() => Guid.NewGuid().ToString("N");
+
+            var sb = new StringBuilder(64);
+            sb.Append(CreateUtf16String());
+
+            var count = length / sb.Length;
+            for (var i = 0; i < count; i++)
+                sb.Append(CreateUtf16String());
+
+            return Encoding.UTF8.GetBytes(sb.ToString().Substring(0, length));
         }
     }
 }
