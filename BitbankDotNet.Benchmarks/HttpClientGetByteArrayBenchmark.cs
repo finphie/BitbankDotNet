72f406deed6dc6d5df8d2927a5aebc3e83a00a9a
finphie <19237170+finphie@users.noreply.github.com>
2018-10-31 19:14:12 +0900

    HTTPリクエストでのbyte配列取得ベンチマーク追加

---
+++ BitbankDotNet.Benchmarks/HttpClientGetByteArrayBenchmark.cs
@@ -0,0 +1,28 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BitbankDotNet.Benchmarks
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Config(typeof(BenchmarkConfig))]
+    public class HttpClientGetByteArrayBenchmark
+    {
+        static readonly Uri Url = new Uri("http://localhost:3000/depth");
+        static readonly HttpClient Client = new HttpClient();
+
+        [Benchmark]
+        public async Task<byte[]> GetByteArrayAsync()
+            => await Client.GetByteArrayAsync(Url);
+
+        [Benchmark]
+        public async Task<byte[]> ReadAsByteArrayAsync()
+        {
+            var response = await Client.SendAsync(new HttpRequestMessage(HttpMethod.Get, Url), HttpCompletionOption.ResponseHeadersRead);
+            return await response.Content.ReadAsByteArrayAsync();
+        }
+    }
+}
