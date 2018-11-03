73b287ea1e213ae33b037df95d131aa9f5a9c42f
finphie <19237170+finphie@users.noreply.github.com>
2018-11-03 10:03:23 +0900

    EnumMember取得に配列を利用するベンチマークを追加

--- BitbankDotNet.Benchmarks/EnumGetEnumMember/EnumGetEnumMemberBenchmark.cs
+++ BitbankDotNet.Benchmarks/EnumGetEnumMember/EnumGetEnumMemberBenchmark.cs
@@ -1,18 +1,30 @@
 ﻿using BenchmarkDotNet.Attributes;
 using EnumsNET;
+using System.Runtime.CompilerServices;
 using System.Runtime.Serialization;
 
 namespace BitbankDotNet.Benchmarks.EnumGetEnumMember
 {
     /// <summary>
-    /// EnumMemberを取得
+    /// EnumMemberを取得する処理のベンチマーク
     /// </summary>
     /// <remarks>
-    /// 
+    /// .NET Core 2.1.5 (CoreCLR 4.6.26919.02, CoreFX 4.6.26919.02), 64bit RyuJIT
+    ///                          Method |          Mean |     Error |    StdDev |  Gen 0 | Allocated |
+    /// ------------------------------- |---------------|-----------|-----------|--------|-----------|
+    ///                        Standard | 2,031.4964 ns | 6.1901 ns | 5.7902 ns | 0.2289 |     360 B |
+    /// ConcurrentDictionaryBaseEnumKey |    39.5527 ns | 0.1766 ns | 0.1652 ns | 0.0152 |      24 B |
+    ///     ConcurrentDictionaryEnumKey |    18.0027 ns | 0.0629 ns | 0.0557 ns |      - |       0 B |
+    ///                       Hashtable |    61.0096 ns | 0.2228 ns | 0.2084 ns | 0.0304 |      48 B |
+    ///                           Array |     0.0016 ns | 0.0044 ns | 0.0039 ns |      - |       0 B |
+    ///                        EnumsNet |    21.3545 ns | 0.0670 ns | 0.0627 ns |      - |       0 B |
     /// </remarks>
     [Config(typeof(BenchmarkConfig))]
     public class EnumGetEnumMemberBenchmark
-    {  
+    {
+        public EnumGetEnumMemberBenchmark()
+            => RuntimeHelpers.RunClassConstructor(typeof(EnumMemberCache<TestEnum>).TypeHandle);
+
         [Benchmark]
         public string Standard()
             => EnumHelper.GetEnumMemberValue(TestEnum.Test1);
@@ -28,6 +40,10 @@ namespace BitbankDotNet.Benchmarks.EnumGetEnumMember
         [Benchmark]
         public string Hashtable()
             => EnumHelperHashtable.GetEnumMemberValue(TestEnum.Test1);
+
+        [Benchmark]
+        public string Array()
+            => EnumMemberCache<TestEnum>.Get(TestEnum.Test1);
 
         [Benchmark]
         public string EnumsNet()
---
+++ BitbankDotNet.Benchmarks/EnumGetEnumMember/EnumMemberCache.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+
+namespace BitbankDotNet.Benchmarks.EnumGetEnumMember
+{
+    static class EnumMemberCache<T>
+        where T: struct, Enum
+    {
+        // ReSharper disable once StaticMemberInGenericType
+        public static readonly string[] Table;
+
+        public static string Get(T value) => Table[Unsafe.As<T, int>(ref value)];
+
+        static EnumMemberCache()
+        {
+            var values = typeof(T).GetFields(BindingFlags.Public | BindingFlags.Static);
+            Table = new string[values.Length];
+            for (var i = 0; i < values.Length; i++)
+                Table[i] = values[i].GetCustomAttribute<EnumMemberAttribute>().Value;
+        }
+    }
+}
