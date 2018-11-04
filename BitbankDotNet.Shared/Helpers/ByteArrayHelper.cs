5c51507bc3c4bf34fc22e843ea2a9b0c1f5d64d5
finphie <19237170+finphie@users.noreply.github.com>
2018-11-04 11:29:00 +0900

    UTF-16文字列作成メソッドを追加

--- BitbankDotNet.Shared/Helpers/ByteArrayHelper.cs
+++ BitbankDotNet.Shared/Helpers/ByteArrayHelper.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Text;
+﻿using System.Text;
 
 namespace BitbankDotNet.Shared.Helpers
 {
@@ -11,18 +10,6 @@ namespace BitbankDotNet.Shared.Helpers
         /// <param name="length">文字列の長さ</param>
         /// <returns>UTF-8のbyte配列を返します。</returns>
         public static byte[] CreateUtf8Bytes(int length)
-        {
-            // UTF-16文字列作成
-            string CreateUtf16String() => Guid.NewGuid().ToString("N");
-
-            var sb = new StringBuilder(64);
-            sb.Append(CreateUtf16String());
-
-            var count = length / sb.Length;
-            for (var i = 0; i < count; i++)
-                sb.Append(CreateUtf16String());
-
-            return Encoding.UTF8.GetBytes(sb.ToString().Substring(0, length));
-        }
+            => Encoding.UTF8.GetBytes(StringHelper.CreateUtf16String(length));
     }
 }
---
+++ BitbankDotNet.Shared/Helpers/StringHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace BitbankDotNet.Shared.Helpers
+{
+    public static class StringHelper
+    {
+        /// <summary>
+        /// UTF-16文字列を作成します。
+        /// </summary>
+        /// <param name="length">文字列の長さ</param>
+        /// <returns>UTF-16文字列を返します。</returns>
+        public static string CreateUtf16String(int length)
+        {
+            // UTF-16文字列作成
+            string CreateUtf16String() => Guid.NewGuid().ToString("N");
+
+            var sb = new StringBuilder(64);
+            sb.Append(CreateUtf16String());
+
+            var count = length / sb.Length;
+            for (var i = 0; i < count; i++)
+                sb.Append(CreateUtf16String());
+
+            return sb.ToString().Substring(0, length);
+        }
+    }
+}
