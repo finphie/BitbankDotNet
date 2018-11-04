f217a3096b1d42af83c8db314f16f28ea0cda641
finphie <19237170+finphie@users.noreply.github.com>
2018-11-04 18:55:30 +0900

    配列の拡張メソッド追加

---
+++ BitbankDotNet.Shared/Extensions/ArrayExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BitbankDotNet.Shared.Extensions
+{
+    /// <summary>
+    /// 配列の拡張メソッド
+    /// </summary>
+    public static class ArrayExtensions
+    {
+        public static void Deconstruct<T>(this T[] array, out T first, out T second, out T third, out T fourth)
+        {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array));
+            if (array.Length < 4)
+                throw new ArgumentOutOfRangeException(nameof(array));
+
+            (first, second, third, fourth) = (array[0], array[1], array[2], array[3]);
+        }
+    }
+}
