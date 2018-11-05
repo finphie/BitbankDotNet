22c6ee0810367cab634c64b0add414d59b03b597
finphie <19237170+finphie@users.noreply.github.com>
2018-11-05 17:17:20 +0900

    リファクタリング

--- BitbankDotNet.Benchmarks/StringConcatBenchmark/BinaryHelper.cs
+++ BitbankDotNet.Benchmarks/StringConcatBenchmark/BinaryHelper.cs
@@ -17,23 +17,29 @@ namespace BitbankDotNet.Benchmarks.StringConcatBenchmark
         public static void Copy(ref char source, ref char destination, int charCount)
         {
             var i = 0;
-            while (charCount >= 4)
+
+            const int count4 = sizeof(long) / sizeof(char);
+            while (charCount >= count4)
             {
                 ref var s = ref Unsafe.As<char, long>(ref Unsafe.Add(ref source, i));
                 ref var d = ref Unsafe.As<char, long>(ref Unsafe.Add(ref destination, i));
                 d = s;
-                i += 4;
-                charCount -= 4;
+                i += count4;
+                charCount -= count4;
             }
-            if (charCount >= 2)
+
+            const int count2 = sizeof(int) / sizeof(char);
+            if (charCount >= count2)
             {
                 ref var s = ref Unsafe.As<char, int>(ref Unsafe.Add(ref source, i));
                 ref var d = ref Unsafe.As<char, int>(ref Unsafe.Add(ref destination, i));
                 d = s;
-                i += 2;
-                charCount -= 2;
+                i += count2;
+                charCount -= count2;
             }
-            if (charCount >= 1)
+
+            const int count1 = sizeof(char) / sizeof(char);
+            if (charCount >= count1)
                 Unsafe.Add(ref destination, i) = Unsafe.Add(ref source, i);
         }
     }
