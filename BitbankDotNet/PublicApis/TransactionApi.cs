d56df4a87b0923957c80b87668492c20aaa6c11b
finphie <19237170+finphie@users.noreply.github.com>
2018-12-03 16:37:23 +0900

    リファクタリング

--- BitbankDotNet/PublicApis/TransactionApi.cs
+++ BitbankDotNet/PublicApis/TransactionApi.cs
@@ -25,20 +25,6 @@ namespace BitbankDotNet
         /// [Public API]指定された日付（UTC）の全約定履歴を返します。
         /// </summary>
         /// <param name="pair">通貨ペア</param>
-        /// <param name="query">クエリ</param>
-        /// <returns>約定履歴</returns>
-        /// <exception cref="BitbankDotNetException">APIリクエストでエラーが発生しました。</exception>
-        async Task<Transaction[]> GetTransactionsAsync(CurrencyPair pair, string query)
-        {
-            var path = TransactionPath + $"/{query}";
-            var result = await PublicApiGetAsync<TransactionList>(path, pair).ConfigureAwait(false);
-            return result.Transactions;
-        }
-
-        /// <summary>
-        /// [Public API]指定された日付（UTC）の全約定履歴を返します。
-        /// </summary>
-        /// <param name="pair">通貨ペア</param>
         /// <param name="year">年</param>
         /// <param name="month">月</param>
         /// <param name="day">日</param>
@@ -66,5 +52,19 @@ namespace BitbankDotNet
         /// <exception cref="BitbankDotNetException">APIリクエストでエラーが発生しました。</exception>
         public Task<Transaction[]> GetTransactionsAsync(CurrencyPair pair, DateTimeOffset date)
             => GetTransactionsAsync(pair, date.UtcDateTime);
+
+        /// <summary>
+        /// [Public API]指定された日付（UTC）の全約定履歴を返します。
+        /// </summary>
+        /// <param name="pair">通貨ペア</param>
+        /// <param name="query">クエリ</param>
+        /// <returns>約定履歴</returns>
+        /// <exception cref="BitbankDotNetException">APIリクエストでエラーが発生しました。</exception>
+        async Task<Transaction[]> GetTransactionsAsync(CurrencyPair pair, string query)
+        {
+            var path = TransactionPath + "/" + query;
+            var result = await PublicApiGetAsync<TransactionList>(path, pair).ConfigureAwait(false);
+            return result.Transactions;
+        }
     }
 }
