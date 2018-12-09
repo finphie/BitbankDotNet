caccf45e36a2f8a4df0d52beddb1024a471d562c
finphie <19237170+finphie@users.noreply.github.com>
2018-12-09 20:50:55 +0900

    SA1027に対処

--- BitbankDotNet.Benchmarks/StringConcat/BinaryHelper.Fast.cs
+++ BitbankDotNet.Benchmarks/StringConcat/BinaryHelper.Fast.cs
@@ -146,7 +146,7 @@ namespace BitbankDotNet.Benchmarks.StringConcat
             ref var s = ref Unsafe.As<char, long>(ref source);
             ref var d = ref Unsafe.As<char, long>(ref destination);
             d = s;
-		}
+        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void CopyChar5(ref char source, ref char destination)
@@ -155,7 +155,7 @@ namespace BitbankDotNet.Benchmarks.StringConcat
             ref var d = ref Unsafe.As<char, long>(ref destination);
             d = s;
             Unsafe.Add(ref destination, 4) = Unsafe.Add(ref source, 4);
-		}
+        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void CopyChar6(ref char source, ref char destination)
@@ -166,7 +166,7 @@ namespace BitbankDotNet.Benchmarks.StringConcat
             s = ref Unsafe.As<char, long>(ref Unsafe.Add(ref source, 2));
             d = ref Unsafe.As<char, long>(ref Unsafe.Add(ref destination, 2));
             d = s;
-		}
+        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void CopyChar7(ref char source, ref char destination)
@@ -177,7 +177,7 @@ namespace BitbankDotNet.Benchmarks.StringConcat
             s = ref Unsafe.As<char, long>(ref Unsafe.Add(ref source, 3));
             d = ref Unsafe.As<char, long>(ref Unsafe.Add(ref destination, 3));
             d = s;
-		}
+        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void CopyChar8(ref char source, ref char destination)
@@ -186,7 +186,7 @@ namespace BitbankDotNet.Benchmarks.StringConcat
             ref var d = ref Unsafe.As<char, long>(ref destination);
             d = s;
             Unsafe.Add(ref d, 1) = Unsafe.Add(ref s, 1);
-		}
+        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void CopyChar9(ref char source, ref char destination)
@@ -196,7 +196,7 @@ namespace BitbankDotNet.Benchmarks.StringConcat
             d = s;
             Unsafe.Add(ref d, 1) = Unsafe.Add(ref s, 1);
             Unsafe.Add(ref destination, 8) = Unsafe.Add(ref source, 8);
-		}
+        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void CopyChar10(ref char source, ref char destination)
@@ -208,7 +208,7 @@ namespace BitbankDotNet.Benchmarks.StringConcat
             s = ref Unsafe.As<char, long>(ref Unsafe.Add(ref source, 6));
             d = ref Unsafe.As<char, long>(ref Unsafe.Add(ref destination, 6));
             d = s;
-		}
+        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void CopyChar11(ref char source, ref char destination)
@@ -220,7 +220,7 @@ namespace BitbankDotNet.Benchmarks.StringConcat
             s = ref Unsafe.As<char, long>(ref Unsafe.Add(ref source, 7));
             d = ref Unsafe.As<char, long>(ref Unsafe.Add(ref destination, 7));
             d = s;
-		}
+        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void CopyChar12(ref char source, ref char destination)
@@ -230,7 +230,7 @@ namespace BitbankDotNet.Benchmarks.StringConcat
             d = s;
             Unsafe.Add(ref d, 1) = Unsafe.Add(ref s, 1);
             Unsafe.Add(ref d, 2) = Unsafe.Add(ref s, 2);
-		}
+        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void CopyChar13(ref char source, ref char destination)
@@ -241,7 +241,7 @@ namespace BitbankDotNet.Benchmarks.StringConcat
             Unsafe.Add(ref d, 1) = Unsafe.Add(ref s, 1);
             Unsafe.Add(ref d, 2) = Unsafe.Add(ref s, 2);
             Unsafe.Add(ref destination, 12) = Unsafe.Add(ref source, 12);
-		}
+        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void CopyChar14(ref char source, ref char destination)
@@ -254,7 +254,7 @@ namespace BitbankDotNet.Benchmarks.StringConcat
             s = ref Unsafe.As<char, long>(ref Unsafe.Add(ref source, 10));
             d = ref Unsafe.As<char, long>(ref Unsafe.Add(ref destination, 10));
             d = s;
-		}
+        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void CopyChar15(ref char source, ref char destination)
@@ -267,7 +267,7 @@ namespace BitbankDotNet.Benchmarks.StringConcat
             s = ref Unsafe.As<char, long>(ref Unsafe.Add(ref source, 11));
             d = ref Unsafe.As<char, long>(ref Unsafe.Add(ref destination, 11));
             d = s;
-		}
+        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void CopyChar16(ref char source, ref char destination)
@@ -278,7 +278,7 @@ namespace BitbankDotNet.Benchmarks.StringConcat
             Unsafe.Add(ref d, 1) = Unsafe.Add(ref s, 1);
             Unsafe.Add(ref d, 2) = Unsafe.Add(ref s, 2);
             Unsafe.Add(ref d, 3) = Unsafe.Add(ref s, 3);
-		}
+        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void CopyChar17(ref char source, ref char destination)
@@ -290,7 +290,7 @@ namespace BitbankDotNet.Benchmarks.StringConcat
             Unsafe.Add(ref d, 2) = Unsafe.Add(ref s, 2);
             Unsafe.Add(ref d, 3) = Unsafe.Add(ref s, 3);
             Unsafe.Add(ref destination, 16) = Unsafe.Add(ref source, 16);
-		}
+        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void CopyChar18(ref char source, ref char destination)
@@ -304,7 +304,7 @@ namespace BitbankDotNet.Benchmarks.StringConcat
             s = ref Unsafe.As<char, long>(ref Unsafe.Add(ref source, 14));
             d = ref Unsafe.As<char, long>(ref Unsafe.Add(ref destination, 14));
             d = s;
-		}
+        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void CopyChar19(ref char source, ref char destination)
@@ -318,7 +318,7 @@ namespace BitbankDotNet.Benchmarks.StringConcat
             s = ref Unsafe.As<char, long>(ref Unsafe.Add(ref source, 15));
             d = ref Unsafe.As<char, long>(ref Unsafe.Add(ref destination, 15));
             d = s;
-		}
+        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void CopyChar20(ref char source, ref char destination)
@@ -330,7 +330,7 @@ namespace BitbankDotNet.Benchmarks.StringConcat
             Unsafe.Add(ref d, 2) = Unsafe.Add(ref s, 2);
             Unsafe.Add(ref d, 3) = Unsafe.Add(ref s, 3);
             Unsafe.Add(ref d, 4) = Unsafe.Add(ref s, 4);
-		}
+        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void CopyChar21(ref char source, ref char destination)
@@ -343,7 +343,7 @@ namespace BitbankDotNet.Benchmarks.StringConcat
             Unsafe.Add(ref d, 3) = Unsafe.Add(ref s, 3);
             Unsafe.Add(ref d, 4) = Unsafe.Add(ref s, 4);
             Unsafe.Add(ref destination, 20) = Unsafe.Add(ref source, 20);
-		}
+        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void CopyChar22(ref char source, ref char destination)
@@ -358,7 +358,7 @@ namespace BitbankDotNet.Benchmarks.StringConcat
             s = ref Unsafe.As<char, long>(ref Unsafe.Add(ref source, 18));
             d = ref Unsafe.As<char, long>(ref Unsafe.Add(ref destination, 18));
             d = s;
-		}
+        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void CopyChar23(ref char source, ref char destination)
@@ -373,7 +373,7 @@ namespace BitbankDotNet.Benchmarks.StringConcat
             s = ref Unsafe.As<char, long>(ref Unsafe.Add(ref source, 19));
             d = ref Unsafe.As<char, long>(ref Unsafe.Add(ref destination, 19));
             d = s;
-		}
+        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void CopyChar24(ref char source, ref char destination)
@@ -386,7 +386,7 @@ namespace BitbankDotNet.Benchmarks.StringConcat
             Unsafe.Add(ref d, 3) = Unsafe.Add(ref s, 3);
             Unsafe.Add(ref d, 4) = Unsafe.Add(ref s, 4);
             Unsafe.Add(ref d, 5) = Unsafe.Add(ref s, 5);
-		}
+        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void CopyChar25(ref char source, ref char destination)
@@ -400,7 +400,7 @@ namespace BitbankDotNet.Benchmarks.StringConcat
             Unsafe.Add(ref d, 4) = Unsafe.Add(ref s, 4);
             Unsafe.Add(ref d, 5) = Unsafe.Add(ref s, 5);
             Unsafe.Add(ref destination, 24) = Unsafe.Add(ref source, 24);
-		}
+        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void CopyChar26(ref char source, ref char destination)
@@ -416,7 +416,7 @@ namespace BitbankDotNet.Benchmarks.StringConcat
             s = ref Unsafe.As<char, long>(ref Unsafe.Add(ref source, 22));
             d = ref Unsafe.As<char, long>(ref Unsafe.Add(ref destination, 22));
             d = s;
-		}
+        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void CopyChar27(ref char source, ref char destination)
@@ -432,7 +432,7 @@ namespace BitbankDotNet.Benchmarks.StringConcat
             s = ref Unsafe.As<char, long>(ref Unsafe.Add(ref source, 23));
             d = ref Unsafe.As<char, long>(ref Unsafe.Add(ref destination, 23));
             d = s;
-		}
+        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void CopyChar28(ref char source, ref char destination)
@@ -446,7 +446,7 @@ namespace BitbankDotNet.Benchmarks.StringConcat
             Unsafe.Add(ref d, 4) = Unsafe.Add(ref s, 4);
             Unsafe.Add(ref d, 5) = Unsafe.Add(ref s, 5);
             Unsafe.Add(ref d, 6) = Unsafe.Add(ref s, 6);
-		}
+        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void CopyChar29(ref char source, ref char destination)
@@ -461,7 +461,7 @@ namespace BitbankDotNet.Benchmarks.StringConcat
             Unsafe.Add(ref d, 5) = Unsafe.Add(ref s, 5);
             Unsafe.Add(ref d, 6) = Unsafe.Add(ref s, 6);
             Unsafe.Add(ref destination, 28) = Unsafe.Add(ref source, 28);
-		}
+        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void CopyChar30(ref char source, ref char destination)
@@ -478,7 +478,7 @@ namespace BitbankDotNet.Benchmarks.StringConcat
             s = ref Unsafe.As<char, long>(ref Unsafe.Add(ref source, 26));
             d = ref Unsafe.As<char, long>(ref Unsafe.Add(ref destination, 26));
             d = s;
-		}
+        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void CopyChar31(ref char source, ref char destination)
@@ -495,7 +495,7 @@ namespace BitbankDotNet.Benchmarks.StringConcat
             s = ref Unsafe.As<char, long>(ref Unsafe.Add(ref source, 27));
             d = ref Unsafe.As<char, long>(ref Unsafe.Add(ref destination, 27));
             d = s;
-		}
+        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void CopyChar32(ref char source, ref char destination)
@@ -510,6 +510,6 @@ namespace BitbankDotNet.Benchmarks.StringConcat
             Unsafe.Add(ref d, 5) = Unsafe.Add(ref s, 5);
             Unsafe.Add(ref d, 6) = Unsafe.Add(ref s, 6);
             Unsafe.Add(ref d, 7) = Unsafe.Add(ref s, 7);
-		}
+        }
     }
 }
