b3e85fcb2bab9766b1db67247cc8cc52bfce1928
finphie <19237170+finphie@users.noreply.github.com>
2019-01-24 19:10:42 +0900

    通貨ペア詳細一覧APIに対応

---
+++ src/BitbankDotNet/PrivateApis/SettingApi.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using BitbankDotNet.Entities;
+
+// ReSharper disable once CheckNamespace
+namespace BitbankDotNet
+{
+    partial class BitbankRestApiClient
+    {
+        const string SettingPath = "/v1/spot/pairs";
+
+        /// <summary>
+        /// [Private API]通貨ペア詳細一覧を返します。
+        /// </summary>
+        /// <returns>通貨ペア詳細一覧</returns>
+        /// <exception cref="BitbankDotNetException">APIリクエストでエラーが発生しました。</exception>
+        public async Task<CurrencyPairSetting[]> GetCurrencyPairSettingsAsync()
+        {
+            var result = await PrivateApiGetAsync<CurrencyPairSettingList>(SettingPath).ConfigureAwait(false);
+            return result.Pairs;
+        }
+    }
+}
